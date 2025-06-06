2716b5bcff762fba46ae9e6dd5db000adb1d0b25
redperiabras <redperiabras@gmail.com>
2025-06-06 21:25:17 +0800

    Update

--- sanctions_pipeline.py
+++ sanctions_pipeline.py
@@ -3,8 +3,8 @@ import dlt
 from dlt.sources.helpers.rest_client import RESTClient
 from dlt.sources.helpers.rest_client.paginators import JSONLinkPaginator
 
-
 base_url = "https://ws-public.interpol.int/notices/v1/un"
+
 
 @dlt.source()
 def src_interpol():
@@ -15,40 +15,42 @@ def src_interpol():
         base_url=base_url,
         headers={
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
-            "Accept": "application/json"
+            "Accept": "application/json",
         },
         paginator=JSONLinkPaginator(next_url_path="_links.next.href"),
     )
 
-    @dlt.resource(file_format="jsonl")
-    def notices():
+    @dlt.resource()
+    def persons():
         """
         Fetches notices from Interpol.
         """
-        for notices in client.paginate('persons'):
-            for notice in notices:
-                yield notice
+        for persons in client.paginate("persons"):
+            for person in persons:
+                yield person
 
     @dlt.transformer(primary_key="entity_id")
-    def transform_notice(item):
+    def person(item):
         """
         Transforms the notices data.
         """
-        path = item['_links']['self']['href']
-        path = path.replace(base_url, '')
-        
+        path = item["_links"]["self"]["href"]
+        path = path.replace(base_url, "")
+
         response = client.get(path).json()
 
         yield response
 
     # Returning the data as a dlt resource
-    return notices | transform_notice
+    return persons | person
+
 
 if __name__ == "__main__":
     pipeline = dlt.pipeline(
-        pipeline_name="interpol_pipeline",
+        pipeline_name="sanctions",
         destination="duckdb",
-        dataset_name="interpol_dataset",
+        dataset_name="interpol",
+        progress=dlt.progress.tqdm(colour="yellow"),
     )
 
     load_info = pipeline.run(src_interpol())
